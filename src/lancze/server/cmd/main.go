874af978e405ba505abf7ef69bf9988411341adc
Piotr Przybylak <piotr.przybylak@gmail.com>
2017-10-26 18:28:29 +0200

    Add home page

--- src/lancze/server/cmd/main.go
+++ src/lancze/server/cmd/main.go
@@ -1,11 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"html/template"
+	"fmt"
+)
 
 func main() {
 	println("Hello!")
+
+	t, err := template.ParseFiles("home.html")
+	if err != nil {
+		fmt.Errorf("Error parsing string", err)
+	}
+
+	type Lunch struct {
+	Place string
+		Name string
+		Price float64
+	}
+
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hejka"))
+		t.Execute(w, []Lunch{{"Restauracja Kolorowa","✔️ consome wołowe z makaronem ryżowym ✔️ pilaw warzywny z kaszy pęczak z dynią", 11}, { "Sznycelek", "Schabowy", 12.99}})
 	})
 	println(http.ListenAndServe(":2233", nil))
 }
