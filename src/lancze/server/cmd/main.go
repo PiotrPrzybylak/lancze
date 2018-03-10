412666501f15aac906bc3372463ff9e4a17dc86b
Piotr Przybylak <piotr.przybylak@gmail.com>
2018-03-10 01:55:44 +0100

    Add bootstrap

--- src/lancze/server/cmd/main.go
+++ src/lancze/server/cmd/main.go
@@ -9,20 +9,28 @@ import (
 func main() {
 	println("Hello!")
 
-	t, err := template.ParseFiles("home.html")
-	if err != nil {
-		fmt.Errorf("Error parsing string", err)
-	}
+
 
 	type Lunch struct {
 	Place string
-		Name string
+		Name template.HTML
 		Price float64
 	}
 
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		t.Execute(w, []Lunch{{"Restauracja Kolorowa","✔️ consome wołowe z makaronem ryżowym ✔️ pilaw warzywny z kaszy pęczak z dynią", 11}, { "Sznycelek", "Schabowy", 12.99}})
+
+		t, err := template.ParseFiles("home.html")
+		if err != nil {
+			fmt.Errorf("Error parsing string", err)
+		}
+
+		t.Execute(w, []Lunch{{"Restauracja Kolorowa","✔️ consome wołowe z makaronem ryżowym ✔️ pilaw warzywny z kaszy pęczak z dynią", 11},
+		{ "Sznycelek", "Schabowy", 12.99},
+		{ "Sznycelek", "Schabowy", 12.99},
+		{ "Sznycelek", "Schabowy", 12.99},
+		{ "Sznycelek", "✔️ zupa pieczarkowa <br/> ✔️ spaghetti di mare", 19.00},
+		{ "Sznycelek", "✔️ zupa pieczarkowa ✔️ spaghetti di mare", 12.99}})
 	})
 	println(http.ListenAndServe(":2233", nil))
 }
