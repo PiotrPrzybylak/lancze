03fc282b73e5d778fee31ec99896cfa630d6caea
Piotr Przybylak <piotr.przybylak@gmail.com>
2018-03-10 18:21:28 +0100

    Simple Admin

--- src/lancze/server/cmd/main.go
+++ src/lancze/server/cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"html/template"
 	"fmt"
+	"strings"
 )
 
 func main() {
@@ -17,6 +18,13 @@ func main() {
 		Price float64
 	}
 
+	lunches := []Lunch{{"Restauracja Kolorowa", "✔️ consome wołowe z makaronem ryżowym ✔️ pilaw warzywny z kaszy pęczak z dynią", 11},
+		{"Sznycelek", "Schabowy", 12.99},
+		{"Sznycelek", "Schabowy", 12.99},
+		{"Sznycelek", "Schabowy", 12.99},
+		{"Sznycelek", "✔️ zupa pieczarkowa <br/> ✔️ spaghetti di mare", 19.00},
+		{"Sznycelek", "✔️ zupa pieczarkowa ✔️ spaghetti di mare", 12.99}}
+
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
@@ -25,12 +33,26 @@ func main() {
 			fmt.Errorf("Error parsing string", err)
 		}
 
-		t.Execute(w, []Lunch{{"Restauracja Kolorowa","✔️ consome wołowe z makaronem ryżowym ✔️ pilaw warzywny z kaszy pęczak z dynią", 11},
-		{ "Sznycelek", "Schabowy", 12.99},
-		{ "Sznycelek", "Schabowy", 12.99},
-		{ "Sznycelek", "Schabowy", 12.99},
-		{ "Sznycelek", "✔️ zupa pieczarkowa <br/> ✔️ spaghetti di mare", 19.00},
-		{ "Sznycelek", "✔️ zupa pieczarkowa ✔️ spaghetti di mare", 12.99}})
+		t.Execute(w, lunches)
 	})
+
+	http.HandleFunc("/admin", func(w http.ResponseWriter, r *http.Request) {
+
+		t, err := template.ParseFiles("admin.html")
+		if err != nil {
+			fmt.Errorf("Error parsing string", err)
+		}
+
+		t.Execute(w, lunches)
+	})
+
+	http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
+r.ParseForm()
+		println(r.Form)
+		println(r.Form.Get("restaurant"))
+		lunches = append(lunches, Lunch{Place: r.Form.Get("restaurant"), Name: template.HTML(strings.Replace(r.Form.Get("menu"), "\n", "<br/>", -1)), Price: 123})
+		http.Redirect(w, r, "/", 301)
+	})
+
 	println(http.ListenAndServe(":2233", nil))
 }
