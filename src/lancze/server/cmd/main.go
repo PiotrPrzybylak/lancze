08dd347c6fec83e3a9001241d740f3595dd6a3c6
Piotr Przybylak <piotr.przybylak@gmail.com>
2018-03-16 00:29:58 +0100

    Escape menu

--- src/lancze/server/cmd/main.go
+++ src/lancze/server/cmd/main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-	"net/http"
-	"html/template"
 	"fmt"
+	"html"
+	"html/template"
+	"net/http"
 	"strings"
 )
 
 func main() {
 	println("Hello!")
 
-
-
 	type Lunch struct {
-	Place string
-		Name template.HTML
+		Place string
+		Name  template.HTML
 		Price float64
 	}
 
@@ -24,7 +23,6 @@ func main() {
 		{"Sznycelek", "Schabowy", 12.99},
 		{"Sznycelek", "✔️ zupa pieczarkowa <br/> ✔️ spaghetti di mare", 19.00},
 		{"Sznycelek", "✔️ zupa pieczarkowa ✔️ spaghetti di mare", 12.99}}
-
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
@@ -47,10 +45,10 @@ func main() {
 	})
 
 	http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
-r.ParseForm()
+		r.ParseForm()
 		println(r.Form)
 		println(r.Form.Get("restaurant"))
-		lunches = append(lunches, Lunch{Place: r.Form.Get("restaurant"), Name: template.HTML(strings.Replace(r.Form.Get("menu"), "\n", "<br/>", -1)), Price: 123})
+		lunches = append(lunches, Lunch{Place: r.Form.Get("restaurant"), Name: template.HTML(strings.Replace(html.EscapeString(r.Form.Get("menu")), "\n", "<br/>", -1)), Price: 123})
 		http.Redirect(w, r, "/", 301)
 	})
 
