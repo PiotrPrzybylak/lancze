c5fb4624ef03df5b30271e064e89a525cde3ca1a
Piotr Przybylak <piotr.przybylak@gmail.com>
2018-03-21 10:41:44 +0100

    save prices

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"strconv"
 )
 
 func main() {
@@ -34,7 +35,7 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-		t, err := template.ParseFiles("lancze/server/home.html")
+		t, err := template.ParseFiles("server/home.html")
 		if err != nil {
 			panic( err)
 		}
@@ -59,7 +60,7 @@ func main() {
 			names = append(names, name)
 		}
 
-		t, err := template.ParseFiles("lancze/server/admin.html")
+		t, err := template.ParseFiles("server/admin.html")
 		if err != nil {
 			panic( err)
 		}
@@ -71,7 +72,16 @@ func main() {
 		r.ParseForm()
 		println(r.Form)
 		println(r.Form.Get("restaurant"))
-		lunches = append(lunches, Lunch{Place: r.Form.Get("restaurant"), Name: template.HTML(strings.Replace(html.EscapeString(r.Form.Get("menu")), "\n", "<br/>", -1)), Price: 123})
+
+		price, err := strconv.ParseFloat(r.Form.Get("price"), 64);
+		if err != nil {
+			panic(err);
+		}
+		lunches = append(lunches,
+			Lunch{
+				Place: r.Form.Get("restaurant"),
+				Name: template.HTML(strings.Replace(html.EscapeString(r.Form.Get("menu")), "\n", "<br/>", -1)),
+				Price: price})
 		http.Redirect(w, r, "/", 301)
 	})
 
